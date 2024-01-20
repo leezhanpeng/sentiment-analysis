cb6c1a2774d344dfd1b664ce40dacc88618bdfd3
zhanpeng <zhanpenglee@outlook.com>
2024-01-20 23:49:15 +0800

    Finalise topic.py and model.py

--- server/model/topic.py
+++ server/model/topic.py
@@ -1,13 +1,44 @@
-from datasets import load_dataset
+from typing import List, Tuple
+import nltk
+from nltk.tokenize import word_tokenize
+from stop_words import get_stop_words
+from nltk.stem import WordNetLemmatizer
+from gensim import corpora
+from gensim.models import LdaModel
 
-dataset = load_dataset("sentence-transformers/reddit-title-body")
-from bertopic import BERTopic
-# Create a BERTopic model
-model = BERTopic(embedding_model="all-MiniLM-L6-v2")
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
 
-print(dataset)
-# Fit the model on your paragraphs
-# topics, probabilities = model.fit_transform(texts)
+stop_words = set(get_stop_words('en'))
+lemmatizer = WordNetLemmatizer()
 
-# print(model.reduce_outliers(texts, topics))
-# print(model.get_topic_info())
+
+def get_topics(input: List[str], num_topics: int) -> List[Tuple[str, int]]:
+    """
+    Generates the topics of given input.
+
+    Parameters:
+        - input (list[string]): The text(s) to derive topics from.
+        - num_topics (int): The number of topics to return.
+
+    Returns:
+        - topics (dict): List of tuples that contain the topic probability pair.
+    """
+    texts = [
+        [lemmatizer.lemmatize(word) for word in word_tokenize(document.lower()) if word.isalnum() and word not in stop_words] for document in input
+    ]
+
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    lda_model = LdaModel(corpus, num_topics=10, id2word=dictionary, passes=15)
+
+    topics = []
+    for topic_id in range(lda_model.num_topics):
+        topic_words = lda_model.show_topic(topic_id)
+        for word, probability in topic_words:
+            topics.append((word, probability))
+
+    topics.sort(key=lambda x: x[1], reverse=True)
+
+    return topics[:num_topics]
