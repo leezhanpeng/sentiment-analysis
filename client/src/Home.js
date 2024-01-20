beb52115531d6bfa97667150d3699efa831aee8d
ElginL <elginlee_2000@hotmail.com>
2024-01-20 14:14:51 +0800

    Add basic text field and send

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 const Home = () => {
     return (
-        <div>
-            hello
-        </div>
+        <Box
+            component="form"
+            sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+        <TextField id="standard-basic" label="Enter your keyword" variant="standard" />
+        <Button variant="contained" endIcon={<SendIcon />}>
+            Send
+        </Button>
+
+      </Box>
     );
 };
 
