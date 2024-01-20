f54cb77404a93beaf98042320a4a00f1c0dc366a
ElginL <elginlee_2000@hotmail.com>
2024-01-20 15:15:53 +0800

    Fix styling

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,20 +3,22 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import '../styles/Form.css';
 
 const Form = () => {
     return (
         <Box
             component="form"
             sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
+            '& > :not(style)': { m: 1, width: '50ch' },
             }}
             noValidate
             autoComplete="off"
+            className="form-container"
         >
-            <TextField id="standard-basic" label="Enter your keyword" variant="standard" />
+            <TextField id="standard-basic" label="Enter your keyword" variant="standard"/>
             <Button variant="contained" endIcon={<SendIcon />}>
-                Send
+                Analyse
             </Button>
         </Box>
     );
