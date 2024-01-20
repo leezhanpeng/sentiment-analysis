f22c59590cb9ce4c96d084acccef0744d4771dee
ElginL <elginlee_2000@hotmail.com>
2024-01-20 14:01:20 +0800

    Remove unnecessary file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
-import TestComp from "./TestComp";
+import Home from "./Home";
+
 class App extends Component {
   render() {
     return (
       <Routes>
-        <Route path="/" element={<TestComp />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     );
   }
